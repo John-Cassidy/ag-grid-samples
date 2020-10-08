c210c8e5782c898b7016057e74a2f72498ca8f09
John Cassidy <jpcassidy@outlook.com>
2020-10-08 14:22:39 -0400

    update simple grid component to make it simple columns

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-home',
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>&nbsp;</p> `,
+  styles: [],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/simplegrid/simplegrid.component.ts
+++ src/app/simplegrid/simplegrid.component.ts
@@ -9,31 +9,46 @@ import { AgGridAngular } from 'ag-grid-angular'
 })
 export class SimplegridComponent implements OnInit {
   @ViewChild('agGrid') agGrid: AgGridAngular
-  title = 'ag-grid-samples'
-  columnDefs = [
-    { headerName: 'Make', field: 'make', rowGroup: true },
-    { headerName: 'Price', field: 'price' },
-  ]
+  columnDefs: any
 
-  autoGroupColumnDef = {
-    headerName: 'Model',
-    field: 'model',
-    cellRenderer: 'agGroupCellRenderer',
-    cellRendererParams: {
-      checkbox: true,
-    },
-  }
   rowData: any
 
-  constructor(private http: HttpClient) {}
+  style = {
+    width: '100%',
+    // height: '100%',
+    flex: '1 1 auto',
+  }
+
+  constructor(private http: HttpClient) {
+    this.columnDefs = [
+      { field: 'athlete', width: 150 },
+      { field: 'age', width: 90 },
+      { field: 'country', width: 150 },
+      { field: 'year', width: 90 },
+      { field: 'date', width: 150 },
+      { field: 'sport', width: 150 },
+      { field: 'gold', width: 100 },
+      { field: 'silver', width: 100 },
+      { field: 'bronze', width: 100 },
+      { field: 'total', width: 100 },
+    ]
+  }
 
   ngOnInit(): void {
     // this.rowData = this.http.get(
     //   'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/smallRowData.json'
     // );
-    this.rowData = this.http.get(
-      'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/rowData.json'
-    )
+    // this.rowData = this.http.get(
+    //   'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/sample-data/rowData.json'
+    // )
+
+    this.http
+      .get(
+        'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinnersSmall.json'
+      )
+      .subscribe((data) => {
+        this.rowData = data
+      })
   }
 
   getSelectedRows() {
